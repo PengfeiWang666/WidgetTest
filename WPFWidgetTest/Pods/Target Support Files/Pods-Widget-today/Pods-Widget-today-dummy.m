f0aded137a641528f37381bbfd4fd9afe90e27dd
WangPengfei <wpf_silence@163.com>
2017-03-23 12:10:29 +0800

    删除忽略文件

---
+++ WPFWidgetTest/Pods/Target Support Files/Pods-Widget-today/Pods-Widget-today-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Widget_today : NSObject
+@end
+@implementation PodsDummy_Pods_Widget_today
+@end
---
+++ WPFWidgetTest/Pods/Target Support Files/Pods-WidgetTest/Pods-WidgetTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WidgetTest : NSObject
+@end
+@implementation PodsDummy_Pods_WidgetTest
+@end
